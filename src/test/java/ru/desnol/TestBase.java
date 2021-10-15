c25e37928a90846d8d5d393bf8e15445d53bceed
MariyaLog7 <Mashyly1994@gmail.com>
2021-10-16 01:21:02 +0300

    update TestBase and build.gradle

--- src/test/java/ru/desnol/TestBase.java
+++ src/test/java/ru/desnol/TestBase.java
@@ -4,20 +4,18 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import io.qameta.allure.selenide.AllureSelenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
-        Configuration.startMaximized = true;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
-           // DesiredCapabilities capabilities = new DesiredCapabilities();
-           // capabilities.setCapability("enableVNC", true);
-           // capabilities.setCapability("enableVideo", true);
-
-            //Configuration.browserCapabilities = capabilities;
-           //Configuration.startMaximized = true;
-           // Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+           capabilities.setCapability("enableVNC", true);
+           capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.startMaximized = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
         }
 
        // @AfterEach
