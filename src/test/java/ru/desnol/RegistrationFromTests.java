d3cb51954e680e2980faff5554197b6581c757db
MariyaLog7 <Mashyly1994@gmail.com>
2021-10-16 00:36:37 +0300

    added allure

---
+++ src/test/java/ru/desnol/RegistrationFromTests.java
@@ -0,0 +1,69 @@
+package ru.desnol;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class RegistrationFromTests extends TestBase {
+
+    Faker faker = new Faker();
+    String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            userEmail = faker.internet().emailAddress(),
+            currentAddress = faker.yoda().quote(),
+            userNumber = "8900369852",
+            gender = "Female",
+            dayOfBirth = "25",
+            monthOfBirth = "August",
+            yearOfBirth = "2020",
+            subject = "Math",
+            hobbies = "Sports",
+            file = "img/1.png",
+            state = "NCR",
+            city = "Delhi",
+            title = "Thanks for submitting the form",
+            picture = "1.png";
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+        $("#genterWrapper").$(byText(gender)).click();
+        $("#userNumber").setValue(userNumber);
+        $("#dateOfBirthInput").clear();
+        $(".react-datepicker__month-select").selectOption(monthOfBirth);
+        $(".react-datepicker__year-select").selectOption(yearOfBirth);
+        $(".react-datepicker__day--0" + dayOfBirth).click();
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $("#hobbiesWrapper").$(byText(hobbies)).click();
+        $("#uploadPicture").scrollTo();
+        $("#uploadPicture").uploadFromClasspath(file);
+        $("#currentAddress").setValue(currentAddress);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText(state)).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText(city)).click();
+        $("#submit").click();
+        $(".modal-title").shouldHave(text(title));
+        $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName));
+        $x("//td[text()='Student Email']").parent().shouldHave(text(userEmail));
+        $x("//td[text()='Gender']").parent().shouldHave(text(gender));
+        $x("//td[text()='Mobile']").parent().shouldHave(text(userNumber));
+        $x("//td[text()='Date of Birth']").parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
+        $x("//td[text()='Subjects']").parent().shouldHave(text(subject));
+        $x("//td[text()='Hobbies']").parent().shouldHave(text(hobbies));
+        $x("//td[text()='Picture']").parent().shouldHave(text(picture));
+        $x("//td[text()='Address']").parent().shouldHave(text(currentAddress));
+        $x("//td[text()='State and City']").parent().shouldHave(text(state + " " + city));
+
+    }
+
+}
+
+
---
+++ src/test/java/ru/desnol/TestBase.java
@@ -0,0 +1,34 @@
+package ru.desnol;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import io.qameta.allure.selenide.AllureSelenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+           // DesiredCapabilities capabilities = new DesiredCapabilities();
+           // capabilities.setCapability("enableVNC", true);
+           // capabilities.setCapability("enableVideo", true);
+
+            //Configuration.browserCapabilities = capabilities;
+           //Configuration.startMaximized = true;
+           // Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        }
+
+       // @AfterEach
+       // public void tearDown() {
+           // Attach.screenshotAs("Last screenshot");
+           // Attach.pageSource();
+           // Attach.browserConsoleLogs();
+           // Attach.addVideo();
+        }
+
+   // }
+//}
+
