9502d4224233859833de2190e23ed00882156da2
MariyaLog7 <Mashyly1994@gmail.com>
2021-10-16 01:37:52 +0300

    addedfile Attach, update TestBase

--- src/test/java/ru/desnol/TestBase.java
+++ src/test/java/ru/desnol/TestBase.java
@@ -1,10 +1,12 @@
 package ru.desnol;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import io.qameta.allure.selenide.AllureSelenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import ru.desnol.helpers.Attach;
 
 public class TestBase {
     @BeforeAll
@@ -18,14 +20,14 @@ public class TestBase {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
         }
 
-       // @AfterEach
-       // public void tearDown() {
-           // Attach.screenshotAs("Last screenshot");
-           // Attach.pageSource();
-           // Attach.browserConsoleLogs();
-           // Attach.addVideo();
+        @AfterEach
+       public void tearDown() {
+        Attach.screenshotAs("My screenshot");
+           Attach.pageSource();
+           Attach.browserConsoleLogs();
+           Attach.addVideo();
         }
 
-   // }
-//}
+    }
+
 
