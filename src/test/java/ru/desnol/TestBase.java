d889e6efca971500342df5bd3ff4e3539d69316d
MariyaLog7 <Mashyly1994@gmail.com>
2021-10-16 14:06:17 +0300

    added SystemPropertiesTests

--- src/test/java/ru/desnol/TestBase.java
+++ src/test/java/ru/desnol/TestBase.java
@@ -1,33 +1,34 @@
 package ru.desnol;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import io.qameta.allure.selenide.AllureSelenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import ru.desnol.helpers.Attach;
+
 
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         DesiredCapabilities capabilities = new DesiredCapabilities();
-           capabilities.setCapability("enableVNC", true);
-           capabilities.setCapability("enableVideo", true);
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
-        }
-
-        @AfterEach
-       public void tearDown() {
-        Attach.screenshotAs("My screenshot");
-           Attach.pageSource();
-           Attach.browserConsoleLogs();
-           Attach.addVideo();
-        }
-
     }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("My screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+
+}
 
 
