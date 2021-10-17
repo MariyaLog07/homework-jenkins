761242ad5cb941b38d2f2b0456196dbb899f4704
MariyaLog7 <Mashyly1994@gmail.com>
2021-10-17 22:29:18 +0300

    added OwnerTests

--- src/test/java/ru/desnol/properties/OwnerTests.java
+++ src/test/java/ru/desnol/properties/OwnerTests.java
@@ -2,19 +2,22 @@ package ru.desnol.properties;
 
 import config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.String.format;
 
-public class OwnerTests {
+    @Tag("properties")
+    public class OwnerTests {
     public CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
+
     @Test
-    void readCredentialsTest(){
+    void readCredentialsTest() {
         String login = credentials.login();
         String password = credentials.password();
         System.out.println(login);
         System.out.println(password);
-        String message = format ("i login as %s with password %s",login,password);
+        String message = format("i login as %s with password %s", login, password);
         System.out.println(message);
     }
 }
